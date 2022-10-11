11434517bb4ce740746560cb5644e3fc27582364
Charles Lowell <cowboyd@frontside.com>
2022-10-11 15:32:40 -0500

    adjust countdown

--- examples/countdown.ts
+++ examples/countdown.ts
@@ -3,7 +3,7 @@ import { run, sleep } from "../mod.ts";
 run(function* () {
   for (let i = 5; i > 0; i--) {
     console.log(`${i}...`);
-    yield* sleep(1000);
+    yield* sleep(800);
   }
   console.log("blastoff!");
 });
