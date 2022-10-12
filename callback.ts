e210c8d6727cbbc5ae87ff66bb061d0e5e151412
Charles Lowell <cowboyd@frontside.com>
2022-10-12 17:23:35 -0500

    Use `Future` in callback

--- callback.ts
+++ callback.ts
@@ -1,40 +1,17 @@
 import type { Operation } from "./types.ts";
-import { action, suspend } from "./instructions.ts";
+import { createFuture } from "./future.ts";
 
 export interface Once<T> extends Operation<T> {
   (value: T): void;
 }
 
 export function callback<T>(): Once<T> {
-  let result: { called: false } | { called: true; value: T } = {
-    called: false,
-  };
 
-  let listeners = new Set<(value: T) => void>();
+  let { resolve, future } = createFuture<T>();
 
-  let callback = (value: T) => {
-    if (!result.called) {
-      result = { called: true, value };
-      for (let listener of listeners) {
-        listener(value);
-      }
-    }
-  };
+  let callback = resolve;
 
   return Object.assign(callback, {
-    *[Symbol.iterator]() {
-      if (result.called) {
-        return result.value;
-      } else {
-        return yield* action<T>(function* (resolve) {
-          try {
-            listeners.add(resolve);
-            yield* suspend();
-          } finally {
-            listeners.delete(resolve);
-          }
-        });
-      }
-    },
+    [Symbol.iterator]: future[Symbol.iterator]
   }) as Once<T>;
 }
