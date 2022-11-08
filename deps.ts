4b877386c204ff3bac7232754297efc4cb1ae7f7
Charles Lowell <cowboyd@frontside.com>
2022-11-08 12:04:10 -0600

    Use the public release of Continuation

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { assert } from "https://deno.land/std@0.158.0/testing/asserts.ts";
-// export * from "https://deno.land/x/continuation@0.1.0/mod.ts";
-export * from "../../@frontside/continuation/mod.ts";
+export * from "https://deno.land/x/continuation@0.1.4/mod.ts";
