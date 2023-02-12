9aa788ec4933fe42df9d5326635d62c8ab215309
Charles Lowell <cowboyd@frontside.com>
2023-02-12 12:15:16 -0600

    ✅Add tests for useAbortSignal() resource

---
+++ test/abort-signal.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect, mock } from "./suite.ts";
+import { run, useAbortSignal } from "../mod.ts";
+
+describe("useAbortSignal()", () => {
+  it("aborts whenever it passes out of scope", async () => {
+    let abort = mock.fn();
+
+    let signal = await run(function*() {
+      let signal = yield* useAbortSignal();
+      signal.addEventListener('abort', abort);
+      expect(signal.aborted).toEqual(false);
+      return signal;
+    });
+    expect(signal.aborted).toBe(true);
+    expect(abort).toHaveBeenCalled();
+  })
+})
--- test/suite.ts
+++ test/suite.ts
@@ -1,5 +1,5 @@
 export * from "https://deno.land/std@0.163.0/testing/bdd.ts";
-export { expect } from "https://deno.land/x/expect@v0.3.0/mod.ts";
+export { expect, mock } from "https://deno.land/x/expect@v0.3.0/mod.ts";
 
 import { Operation, sleep } from "../mod.ts";
 
