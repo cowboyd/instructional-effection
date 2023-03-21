1861ee084f308076936f7de70226827f11a19731
Eric Bower <me@erock.io>
2023-03-21 11:57:34 -0400

    feat: npm package

---
+++ npm.ts
@@ -0,0 +1,40 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.17.0/mod.ts";
+import { assert } from "https://deno.land/std@0.129.0/testing/asserts.ts";
+await emptyDir("./npm");
+
+let version = Deno.env.get("NPM_VERSION");
+assert(version, "NPM_VERSION is required to build npm package");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: false,
+  },
+  test: false,
+  typeCheck: false,
+  compilerOptions: {
+    target: "ES2020",
+    sourceMap: true,
+  },
+  package: {
+    // package.json properties
+    name: "@frontside/effection-v3",
+    version,
+    description: "Delimited continuations for JavaScript",
+    license: "MIT",
+    repository: {
+      author: "engineering@frontside.com",
+      type: "git",
+      url: "git+https://github.com/thefrontside/effection.git",
+    },
+    bugs: {
+      url: "https://github.com/thefrontside/effection/issues",
+    },
+    engines: {
+      node: ">= 14",
+    },
+  },
+});
+
+await Deno.copyFile("README.md", "npm/README.md");
